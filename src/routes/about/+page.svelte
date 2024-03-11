01bd0d888dc146a16bacf51121534221283af4a6
Faytix <za.sorokin2017@yandex.ru>
2024-03-11 21:19:27 +0300

    rr

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,57 @@
-<h1>Обо мне</h1>
+<svelte:head>
+<title>Путешествия</title>
+</svelte:head>
+<div class="container">
+    <h1>Места из моих путешествий</h1>
+    <h4>Прошлые 2 года были весьма интересными и я хотел бы поделиться своими фотографиями с вами</h4>
+    <div class="blogposts">
+        {#each routes as page}
+            <div class="post">
+                <h2>{page.title}</h2>
+                <p>{page.body}</p>
+                <p class="readmore">
+                    Для просмотра фото 
+                    <a class="link" href={`/about/travel/${page.id}`}>
+                        Click here
+                    </a>
+                </p>
+            </div>
+        {/each}
+    </div>
+</div>
 
-todo...
+<style>
+    .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+
+    .blogposts {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+
+    .post {
+        border: 1px solid #8b8787;
+        padding: 10px;
+        box-shadow: 0 0 10px #eee;
+        color: #1c856a;
+    }
+
+    .link {
+        color:rgb(10, 10, 139);
+        border: 2px solid rgb(189, 185, 185);
+        border-radius: 10px;           
+    }
+    .link:hover {
+        border-color: red;
+    }
+
+</style>
+
+
+<script>
+    import {routes} from './routes'
+</script>
---
+++ src/routes/about/travel/2/+page.svelte
@@ -0,0 +1,8 @@
+<h1>В 2023 я никуда не ездил, так что хочу просто показать красивые места моего города в которых я успел побывать</h1>
+<div class="images">
+    <img src="https://yastatic.net/naydex/yandex-search/y8hYCH729/4eec8ajbyW/_zftdZ28RkNLpO85TLrkLvU-BWCEfRexqkjKaJ2h4KFSzeL8NSaHUJ1ZtzB87HxFFGQ973A6U7kMNgBfXZurxrti-Dna_VoeFuH5bUS54SzoY0ZxmCcT7LAKljHZquirWp-JxxdUpmqxPp1AU5AzEUJka6VrfhTTZw" alt="Image1">
+    <img src="https://yastatic.net/naydex/yandex-search/y8hYCH781/4eec8ajbyW/_zftdZ28RkNLpO85TLrkLvU-BWCEfRexqkjKbZH1tqAIzuGvbyXTAM9e5D4u7nxBGzU57HA8BLRbNg4NAJL3wL1v-zne_VsYHuDxeAC97SrqdRgohDoL46Ff0T3E4b34Vp3lxwlUpXehI4ZOXZcECU1uarg9IhP5Z1k" alt="Image2">
+    <img src="https://yastatic.net/naydex/yandex-search/y8hYCH781/4eec8ajbyW/_zftdZ28RkNLpO85TLrkLvU-BWCEfRexqkjKapmjvqINmbf9MCfdDJtbsTMt7XoVTTc57Cc_B7lbZQ9fXZCrlu01rjne_1kbFuTzcgC97SrqdRgohDoL46Ff0T3E4b34Vp3lxwlUpXehI4ZOXZcECU1uarg9IhP5Z1k" alt="Image3">
+    <img src="https://yastatic.net/naydex/yandex-search/y8hYCH729/4eec8ajbyW/_zftdZ28RkNLpO85TLrkLvU-BWCEfRexqkjKas2h4qFdzeOpNCKHVslYtT8ovXNETTY97XVvULoKMgZaCserwL9v-DnX9l4dFuTybUS54SzoY0ZxmCcT7LAKljHZquirWp-JxxdUpmqxPp1AU5AzEUJka6VrfhTTZw" alt="Image4">
+    <img src="https://yastatic.net/naydex/yandex-search/y8hYCH781/4eec8ajbyW/_zftdZ28RkNLpO85TLrkLvU-BWCEfRexqkjKOczwtfAPzuKgNXDSAJgO4jYs7y4RGjY75Cc4B7QMPg4NXMfxz7hnoDne_V0fF-L3eQC97SrqdRgohDoL46Ff0T3E4b34Vp3lxwlUpXehI4ZOXZcECU1uarg9IhP5Z1k" alt="Image5">
+</div>
